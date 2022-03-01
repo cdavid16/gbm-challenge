77bf3fa409703ed124449041cb28264d7447d079
c_david <david.villarreal17@gmail.com>
2022-02-28 23:41:22 -0600

    missing file.

--- src/main/java/com/gbm/challenge/gbmchallenge/GbmChallengeApplication.java
+++ src/main/java/com/gbm/challenge/gbmchallenge/GbmChallengeApplication.java
@@ -2,13 +2,8 @@ package com.gbm.challenge.gbmchallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude = {
-//		DataSourceAutoConfiguration.class,
-//		DataSourceTransactionManagerAutoConfiguration.class,
-//		HibernateJpaAutoConfiguration.class})
 public class GbmChallengeApplication {
 
 	public static void main(String[] args) {
